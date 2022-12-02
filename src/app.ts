8242261ccc87c69e9b862a6fe3d998cd38dd34e2
Kacper Falecki <kfalecki17@gmail.com>
2022-12-02 01:10:59 +0100

    first commit

---
+++ src/app.ts
@@ -0,0 +1,41 @@
+const container = document.querySelector('.result')!;
+
+const renderTable = async () => {
+    const usersURL: string = 'http://localhost:3500/users';
+    const companiesURL: string = 'http://localhost:3500/companies';
+    const userData: Response = await fetch(usersURL);
+    const companiesData: Response = await fetch(companiesURL);
+
+    interface CompanyUser{
+        company: string
+    }
+    interface ListUser{
+        email: string;
+        name: string;
+        uri: string;
+        uris: CompanyUser
+    }
+    const userList: ListUser[] = await userData.json();
+    interface ListCompany{
+        name: string;
+        uri: string
+    }
+    const compList: ListCompany[] = await companiesData.json();
+    let template: string
+    template = '';
+        for(var i of compList){
+            template += '<tr><td>';
+            template += i.name;
+            template += '</td><td>';
+            for(var j of userList){
+                if(j.uris.company === i.uri){
+                    template += j.name + '<br>';
+                }
+            }
+            template += "</td></tr>";
+        }
+        container.innerHTML = template;
+    
+    console.log(compList);
+};
+window.addEventListener("DOMContentLoaded", () => renderTable());
