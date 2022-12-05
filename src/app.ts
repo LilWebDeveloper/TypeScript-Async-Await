31fa32715c01dab69927fe60db37c83928ee7fb2
Kacper Falecki <kfalecki17@gmail.com>
2022-12-05 12:17:34 +0100

    Add async function type

--- src/app.ts
+++ src/app.ts
@@ -1,22 +1,23 @@
+interface CompanyUser{
+    company: string;
+};
+interface ListCompany{
+    name: string;
+    uri: string;
+};
+interface ListUser extends ListCompany{
+    email: string;
+    uris: CompanyUser;
+};
+
 const container = document.querySelector('.result')! as HTMLTableElement;
-const renderTable = async () => {
+async function renderTable(): Promise<void> {
     const usersURL: string = 'http://localhost:3500/users';
-    const companiesURL: string = 'http://localhost:3500/companies';
     const userData: Response = await fetch(usersURL);
+    const userList: ListUser[] = await userData.json();
+    const companiesURL: string = 'http://localhost:3500/companies';
     const companiesData: Response = await fetch(companiesURL);
-    interface CompanyUser{
-        company: string;
-    };
-    interface ListUser extends ListCompany{
-        email: string;
-        uris: CompanyUser;
-    };
-    const userList: Array<ListUser> = await userData.json();
-    interface ListCompany{
-        name: string;
-        uri: string;
-    };
-    const compList: Array<ListCompany> = await companiesData.json();
+    const compList: ListCompany[] = await companiesData.json();
     let template: string;
     template = '';
         for(var i of compList){
