6362bc3886989b5ed76c4d3ee7586face6b0075a
Kacper Falecki <kfalecki17@gmail.com>
2022-12-05 12:18:28 +0100

    Change const name

--- src/app.ts
+++ src/app.ts
@@ -14,15 +14,15 @@ const container = document.querySelector('.result')! as HTMLTableElement;
 async function renderTable(): Promise<void> {
     const usersURL: string = 'http://localhost:3500/users';
     const userData: Response = await fetch(usersURL);
-    const userList: ListUser[] = await userData.json();
+    const user: ListUser[] = await userData.json();
     const companiesURL: string = 'http://localhost:3500/companies';
     const companiesData: Response = await fetch(companiesURL);
-    const compList: ListCompany[] = await companiesData.json();
+    const companies: ListCompany[] = await companiesData.json();
     let template: string;
     template = '';
-        for(var i of compList){
+        for(var i of companies){
             template += '<tr><td>' + i.name + '</td><td>';
-            for(var j of userList){
+            for(var j of user){
                 if(j.uris.company === i.uri){
                     template += j.name + '<br>';
                 }
